a50ae13cbdaadfffcece82b522f56fdbe01180dd
Waqas Ali <66446805+waqas-cs@users.noreply.github.com>
2022-06-22 02:39:58 +0500

    responsive complete

--- src/components/InnerContent/index.js
+++ src/components/InnerContent/index.js
@@ -1,35 +1,41 @@
-import React, {useState}from 'react'
-import AddBeneficiary from '../AddBeneficiary'
-import BeneficiaryForm from '../AddBeneficiary/BeneficiaryForm'
-import './styles.scss'
-import data from './data.json';
-import {useDispatch, useSelector} from 'react-redux';
-import TransferDetails from '../TransferDetails';
-import CollectionPoint from '../CollectionPoint';
-import PaymentMethod from '../PaymentMethod';
-import ReviewPayment from '../ReviewPayment';
-import OtpConfirmation from '../OtpConfirmation';
-const InnerContent = ({setAddedBenData, collectionPoint, transferDetails, paymentMethod, reviewPayment}) => {
-  const darkMode = useSelector(state=>state.themeActions.darkMode)
-    const dispatch = useDispatch();
-  const [addBen, setAddBen] = useState(false)
-  const [ben, setBen] = useState(false)
-  const [addBenData, setAddBenData] = useState(false)
-  const [otp, setOtp] = useState(false)
+import React, { useState } from "react";
+import AddBeneficiary from "../AddBeneficiary";
+import BeneficiaryForm from "../AddBeneficiary/BeneficiaryForm";
+import "./styles.scss";
+import data from "./data.json";
+import { useDispatch, useSelector } from "react-redux";
+import TransferDetails from "../TransferDetails";
+import CollectionPoint from "../CollectionPoint";
+import PaymentMethod from "../PaymentMethod";
+import ReviewPayment from "../ReviewPayment";
+import OtpConfirmation from "../OtpConfirmation";
+const InnerContent = ({
+  setAddedBenData,
+  collectionPoint,
+  transferDetails,
+  paymentMethod,
+  reviewPayment,
+}) => {
+  const darkMode = useSelector((state) => state.themeActions.darkMode);
+  const dispatch = useDispatch();
+  const [addBen, setAddBen] = useState(false);
+  const [ben, setBen] = useState(false);
+  const [addBenData, setAddBenData] = useState(false);
+  const [otp, setOtp] = useState(false);
 
-  if(ben){
-    setAddedBenData("transaction")
+  if (ben) {
+    setAddedBenData("transaction");
   }
   return (
-    <div className={`inner-content ${darkMode?'dark-mode' : ''} `}>
+    <div className={`inner-content ${darkMode ? "dark-mode" : ""} `}>
       {/***************** test code start */}
       {/* <OtpConfirmation/> */}
-        {/* <ReviewPayment/> */}
-        {/* <PaymentMethod/> */}
-        {/* <CollectionPoint/> */}
+      {/* <ReviewPayment/> */}
+      <PaymentMethod />
+      {/* <CollectionPoint /> */}
       {/* <TransferDetails/> */}
       {/*****************  test code end */}
-      {!addBen?
+      {/* {!addBen?
       <AddBeneficiary setAddBen = {(val)=>{setAddBen(val)}}/>
     :!ben?
       <BeneficiaryForm setBen = {(val)=>{setBen(val)}} />
@@ -48,9 +54,9 @@ const InnerContent = ({setAddedBenData, collectionPoint, transferDetails, paymen
       <ReviewPayment  setOtp ={(val)=>{setOtp(val)}}/>
         :
       <OtpConfirmation />
-      }
+      } */}
     </div>
-  )
-}
+  );
+};
 
-export default InnerContent
+export default InnerContent;
