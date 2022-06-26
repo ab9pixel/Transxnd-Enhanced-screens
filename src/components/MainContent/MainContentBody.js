625ad93406ef85a797a35be8293166095f9c7d5b
Waqas Ali <66446805+waqas-cs@users.noreply.github.com>
2022-06-26 11:54:32 +0500

    removed unused lines

--- src/components/MainContent/MainContentBody.js
+++ src/components/MainContent/MainContentBody.js
@@ -1,34 +1,61 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./styles.scss";
 import MoneyCalculator from "./MoneyCalculator";
 import InnerSidebar from "../InnerSidebar";
 import InnerContent from "../InnerContent";
 import SupportMessage from "../SupportMessage";
 const MainContentBody = () => {
-  const [progress, setProgress] = useState(1)
-  const [sendMoneyNow, setSendMoneyNow] = useState (false)
-  const [addedBen, setAddedBenData] = useState("")
-  const [collectionPoint, setCollectionPoint] = useState(false)
-  const [paymentMethod, setPaymentMethod] = useState(false)
-  const [transferDetails, setTransferDetails] = useState(false)
-  const [reviewPayment, setReviewPayment] = useState(false)
+  const [progress, setProgress] = useState(1);
+  const [sendMoneyNow, setSendMoneyNow] = useState(false);
+  const [addedBen, setAddedBenData] = useState("");
+  const [collectionPoint, setCollectionPoint] = useState(false);
+  const [paymentMethod, setPaymentMethod] = useState(false);
+  const [transferDetails, setTransferDetails] = useState(false);
+  const [reviewPayment, setReviewPayment] = useState(false);
   return (
     <div className="MainContentBody">
       <div className="MainContentBodyWrap">
         {/* TEST CODE */}
-      {/* END TEST CODE */}
-        {
-          !sendMoneyNow ? <MoneyCalculator setSendMoneyNow={setSendMoneyNow}/> :
-        <div className = "SendMoneyWrap">
-          <InnerSidebar progress = {progress} setProgress = {(val)=> {setProgress(val)}} />
-          <InnerContent progress = {progress} reviewPayment={reviewPayment} setAddedBenData={setAddedBenData} transferDetails={transferDetails} setProgress = {(val)=> {setProgress(val)}} collectionPoint={collectionPoint} paymentMethod={paymentMethod} />
-        </div>
-        }
-        <SupportMessage buttonLink={addedBen} transferDetails={transferDetails} reviewPayment={reviewPayment} setReviewPayment={(val)=>setReviewPayment(val)} setTransferDetails={(val)=>{setTransferDetails(true)}} collectionPoint={collectionPoint} setCollectionPoint= {(val)=>setCollectionPoint(val)}
-        paymentMethod = {paymentMethod}
-        setPaymentMethod={(val)=>{setPaymentMethod(val)}} 
+        {/* END TEST CODE */}
+        {!sendMoneyNow ? (
+          <MoneyCalculator setSendMoneyNow={setSendMoneyNow} />
+        ) : (
+          <div className="SendMoneyWrap">
+            <InnerSidebar
+              progress={progress}
+              setProgress={(val) => {
+                setProgress(val);
+              }}
+            />
+            <InnerContent
+              progress={progress}
+              reviewPayment={reviewPayment}
+              setAddedBenData={setAddedBenData}
+              transferDetails={transferDetails}
+              setProgress={(val) => {
+                setProgress(val);
+              }}
+              collectionPoint={collectionPoint}
+              paymentMethod={paymentMethod}
+            />
+          </div>
+        )}
+        <SupportMessage
+          buttonLink={addedBen}
+          transferDetails={transferDetails}
+          reviewPayment={reviewPayment}
+          setReviewPayment={(val) => setReviewPayment(val)}
+          setTransferDetails={(val) => {
+            setTransferDetails(true);
+          }}
+          collectionPoint={collectionPoint}
+          setCollectionPoint={(val) => setCollectionPoint(val)}
+          paymentMethod={paymentMethod}
+          setPaymentMethod={(val) => {
+            setPaymentMethod(val);
+          }}
         />
-      </div>        
+      </div>
     </div>
   );
 };
