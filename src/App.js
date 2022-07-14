76ab9d20f1b81dd07e04b721e5d6ee897e402109
Hammad39 <hammadimran3@gmail.com>
2022-07-14 13:28:51 +0500

    final styling updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Home from "./pages/home.js";
 import './App.scss'
+import { useSelector } from "react-redux";
+
 function App() {
+  const darkMode = useSelector(state=>state.themeActions.darkMode)
   return (
-    <div className="App">
+    <div className={`App ${darkMode? 'dark-mode' :''}`}>
       <Home/>
     </div>
   );
